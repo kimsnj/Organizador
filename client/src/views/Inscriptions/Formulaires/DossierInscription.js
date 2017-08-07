521851aeecef3cae935b59cb7eb34cc4dcef3698
Karim Snj <karim.snj@gmail.com>
2017-08-07 22:13:13 +0200

    WIP moving Inscription form to redux

--- client/src/views/Inscriptions/Formulaires/DossierInscription.js
+++ client/src/views/Inscriptions/Formulaires/DossierInscription.js
@@ -1,55 +1,34 @@
 import React, { Component } from 'react';
+import { Field } from 'redux-form'
 
-class DossierInscription extends Component {
-    constructor(props) {
-        super(props);
-        this.onDossierInscriptionChanged = this.onDossierInscriptionChanged.bind(this);
-        this.state = {
-            certifMed: false,
-            photoId: false,
-            ficheAdhesion: false
-        };
-    }
+let CheckBoxField = field => (
+    <div className="form-check">
+        <label className="form-check-label">
+            <input className="form-check-input" style={{ marginRight: 5 + 'px' }} type="checkbox" {...field.input} />
+            {field.label}
+        </label>
+    </div>
+);
 
-    onDossierInscriptionChanged(e) {
-        console.log('onDossierInscriptionChanged');
-    }
-
-    render() {
-        return (      
-            <div className="row">
-                <div className="col-md-12">
-                    <div className="card">
-                        <div className="card-header">
-                            <h4 className="card-title mb-0"><i className="fa fa-check-square-o"></i> Dossier d'inscription</h4>
-                        </div>
-                        <div className="card-block">
-                            <fieldset className="form-group" style={{marginLeft: 30 + 'px'}}>
-                                <div className="form-check">
-                                <label className="form-check-label">
-                                    <input type="checkbox" style={{marginRight: 5 + 'px'}} className="form-check-input" name="dossierInscriptionCheckBoxes" id="dossierCheckBox1" value={this.state.certifMed} onChange={this.onDossierInscriptionChanged}/>
-                                    Certificat médical
-                                </label>
-                                </div>
-                                <div className="form-check">
-                                <label className="form-check-label">
-                                    <input type="checkbox" style={{marginRight: 5 + 'px'}} className="form-check-input" name="dossierInscriptionCheckBoxes" id="dossierCheckBox2" value={this.state.photoId} onChange={this.onDossierInscriptionChanged}/>
-                                    Photo d'identité
-                                </label>
-                                </div>
-                                <div className="form-check">
-                                <label className="form-check-label">
-                                    <input type="checkbox" style={{marginRight: 5 + 'px'}} className="form-check-input" name="dossierInscriptionCheckBoxes" id="dossierCheckBox3" value={this.state.ficheAdhesion} onChange={this.onDossierInscriptionChanged}/>
-                                    Fiche d'adhésion
-                                </label>
-                                </div>
-                            </fieldset>
-                        </div>
+let DossierInscription = (props) => {
+    return (
+        <div className="row">
+            <div className="col-md-12">
+                <div className="card">
+                    <div className="card-header">
+                        <h4 className="card-title mb-0"><i className="fa fa-check-square-o"></i> Dossier d'inscription</h4>
+                    </div>
+                    <div className="card-block">
+                        <fieldset className="form-group" style={{ marginLeft: 30 + 'px' }}>
+                            <Field name="certificat" label="Certificat Médical" component={CheckBoxField} />
+                            <Field name="photo" label="Photo d'identité" component={CheckBoxField} />
+                            <Field name="fiche" label="Fiche d'adhésion" component={CheckBoxField} />
+                        </fieldset>
                     </div>
                 </div>
             </div>
-        )
-  }
+        </div>
+    )
 }
 
 export default DossierInscription;
--- client/src/views/Inscriptions/Formulaires/InfosCapoeira.js
+++ client/src/views/Inscriptions/Formulaires/InfosCapoeira.js
@@ -1,140 +1,136 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux'
+import { Field, formValueSelector } from 'redux-form'
 
-var COURS = {
-  '1': {id: 1, jour: 'Lundi', horaire: '17h-18h30', lieu: 'Ranguin', type: 'Adolescents'},
-  '2': {id: 2, jour: 'Lundi', horaire: '20h-21h30', lieu: 'Ranguin', type: 'Adultes'},
-  '3': {id: 3, jour: 'Mercredi', horaire: '18h30-19h30', lieu: 'Pégomas', type: 'Ados'},
-  '4': {id: 4, jour: 'Mercredi', horaire: '17h-18h15', lieu: 'Auribeau', type: 'Enfants'},
-  '5': {id: 5, jour: 'Jeudi', horaire: '17h-18h', lieu: 'Pégomas', type: 'Eveil'},
-  '6': {id: 6, jour: 'Jeudi', horaire: '18h-19h30', lieu: 'Pégomas', type: 'Enfants'},
-  '7': {id: 7, jour: 'Jeudi', horaire: '20h-21h30', lieu: 'Auribeau', type: 'Adultes'},
-  '8': {id: 8, jour: 'Vendredi', horaire: '17h-18h', lieu: 'Pégomas', type: 'Eveil'},
-  '9': {id: 9, jour: 'Vendredi', horaire: '18h-19h', lieu: 'Pégomas', type:  'Roda'}
-};
+var COURS = [
+    { id: 1, jour: 'Lundi', horaire: '17h-18h30', lieu: 'Ranguin', type: 'adolescents' },
+    { id: 2, jour: 'Lundi', horaire: '20h-21h30', lieu: 'Ranguin', type: 'adultes' },
+    { id: 3, jour: 'Mercredi', horaire: '18h30-19h30', lieu: 'Pégomas', type: 'adolescents' },
+    { id: 4, jour: 'Mercredi', horaire: '17h-18h15', lieu: 'Auribeau', type: 'enfants' },
+    { id: 5, jour: 'Jeudi', horaire: '17h-18h', lieu: 'Pégomas', type: 'eveil' },
+    { id: 6, jour: 'Jeudi', horaire: '18h-19h30', lieu: 'Pégomas', type: 'enfants' },
+    { id: 7, jour: 'Jeudi', horaire: '20h-21h30', lieu: 'Auribeau', type: 'adultes' },
+    { id: 8, jour: 'Vendredi', horaire: '17h-18h', lieu: 'Pégomas', type: 'eveil' },
+    { id: 9, jour: 'Vendredi', horaire: '18h-19h', lieu: 'Pégomas', type: 'all' }
+];
 
-class InfosCapoeira extends Component {
-    constructor(props) {
-        super(props);
-        this.onCoursChanged = this.onCoursClicked.bind(this);
-        this.onTypeCoursChanged = this.onTypeCoursChanged.bind(this);
-        this.onCordaSelected = this.onCordaSelected.bind(this);
-        this.onTailleAbadaChanged = this.onTailleAbadaChanged.bind(this);
-    }
+let filtered_courses = (categorie) => (
+    COURS.filter((cours) => (
+        cours.type == categorie || cours.type == 'all'
+    ))
+)
 
-    onTypeCoursChanged(e) {
-        console.log('onTypeCoursChanged'); // TODO sort...
-    }
+/*
+FONCTION ANONYME
 
-    onCoursClicked(e) {
-        console.log('onCoursClicked');
-    }
+(c) => (
+        c.type == categorie || c.type == 'all'
+    )
 
-    onCordaSelected(e) {
-        console.log('onCordaSelected');
-    }
+EQUIVALENT TO
 
-    onTailleAbadaChanged(e) {
-        console.log('onTailleAbadaChanged ' + e.target.value);
-    }
+function (c) {
+    return c.type == categorie || c.type == 'all';
+}
 
-    render() {
-        return (
-            <div className="row">
-                <div className="col-md-12">
-                    <div className="card">
-                        <div className="card-header">
-                            <h4><i className="fa fa-mortar-board"></i> Informations cours</h4>
+*/
+
+let InfosCapoeira = ({ categorie }) => (
+    <div className="row">
+        <div className="col-md-12">
+            <div className="card">
+                <div className="card-header">
+                    <h4><i className="fa fa-mortar-board"></i> Informations cours</h4>
+                </div>
+                <div className="card-block">
+                    <div className="form-group row">
+                        <label className="col-md-3 form-control-label" htmlFor="text-input">Apellido</label>
+                        <div className="col-md-9">
+                            <Field component="input"
+                                type="text"
+                                id="text-input"
+                                name="apellido"
+                                className="form-control"
+                                placeholder="Champ optionnel" />
                         </div>
-                        <div className="card-block">
-                            <form
-                                action=""
-                                method="post"
-                                encType="multipart/form-data"
-                                className="form-horizontal">
-                                <div className="form-group row">
-                                    <label className="col-md-3 form-control-label" htmlFor="text-input">Apellido</label>
-                                    <div className="col-md-9">
-                                        <input
-                                            type="text"
-                                            id="text-input"
-                                            name="text-input"
-                                            className="form-control"
-                                            placeholder="Champ optionnel"/>
-                                    </div>
-                                </div>
-                                <div className="form-group row">
-                                    <label className="col-md-3 form-control-label">Type de cours</label>
-                                    <div className="col-md-9">
-                                        <label className="radio-inline" htmlFor="inline-radio1">
-                                            <input type="radio" id="inline-radio1" name="inline-radios" value="eveil" onChange={this.onTypeCoursChanged}/> Eveil 
-                                        </label>
-                                        <label className="radio-inline" htmlFor="inline-radio2" style={{marginLeft: 5 + 'px'}}>
-                                            <input type="radio" id="inline-radio2" name="inline-radios" value="enfants" onChange={this.onTypeCoursChanged}/> Enfants 
-                                        </label>
-                                        <label className="radio-inline" htmlFor="inline-radio3" style={{marginLeft: 5 + 'px'}}>
-                                            <input type="radio" id="inline-radio3" name="inline-radios" value="adolescents" onChange={this.onTypeCoursChanged}/> Adolescents 
-                                        </label>
-                                        <label className="radio-inline" htmlFor="inline-radio3" style={{marginLeft: 5 + 'px'}}>
-                                            <input type="radio" id="inline-radio3" name="inline-radios" value="adultes" onChange={this.onTypeCoursChanged}/> Adultes 
-                                        </label>
-                                    </div>
-                                </div>
-                                <div className="form-group row">
-                                    <label className="col-md-3 form-control-label" htmlFor="multiple-select">Cours</label>
-                                    <div className="col-md-9">
-                                    <select id="multiple-select" name="multiple-select" className="form-control" size="5" multiple>
-                                        <option value="1" onClick={this.onCoursClicked}>TODO liste intelligente en fonction du type de cours choisi</option>
-                                        <option value="2" onClick={this.onCoursClicked}>Lundi 17h-18h30 Ranguin Adolescents</option>
-                                        <option value="3" onClick={this.onCoursClicked}>Lundi 20h-21h30 Ranguin Adultes</option>
-                                        <option value="4" onClick={this.onCoursClicked}>Mercredi 18h30-19h30 Pégomas Ados</option>
-                                        <option value="5" onClick={this.onCoursClicked}>Mercredi 17h-18h15 Auribeau Enfants débutants</option>
-                                        <option value="6" onClick={this.onCoursClicked}>Jeudi 17h-18h Pégomas Eveil</option>
-                                        <option value="7" onClick={this.onCoursClicked}>Jeudi 18h-19h30 Pégomas Enfants avancés</option>
-                                        <option value="8" onClick={this.onCoursClicked}>Jeudi 20-21h30 Auribeau Adultes</option>
-                                        <option value="9" onClick={this.onCoursClicked}>Vendredi 17h-18h Pégomas Eveil</option>
-                                        <option value="10" onClick={this.onCoursClicked}>Vendredi 18h-19h Pégomas Roda</option>
-                                    </select>
-                                    </div>
-                                </div>
-                                <div className="form-group row">
-                                    <label className="col-md-3 form-control-label" htmlFor="select">Corde</label>
-                                    <div className="col-md-9">
-                                        <select id="select" name="select" className="form-control">
-                                            <option value="0" onClick={this.onCordaSelected}>Dérouler...</option>
-                                            <option value="semCorda" onClick={this.onCordaSelected}>Sem corda</option>
-                                            <option value="azul" onClick={this.onCordaSelected}>Azul</option>
-                                            <option value="laranja" onClick={this.onCordaSelected}>Laranja</option>
-                                            <option value="laranjaMarron" onClick={this.onCordaSelected}>Laranja Marron</option>
-                                            <option value="marron" onClick={this.onCordaSelected}>Marron</option>
-                                            <option value="marronRoxa" onClick={this.onCordaSelected}>Marron Roxa</option>
-                                        </select>
-                                        <span className="help-block">TODO : changer la liste en fonction du type de cours</span>
-                                    </div>
-                                </div>
-                                <div className="form-group row">
-                                    <label className="col-md-3 form-control-label">Taille d'abada</label>
-                                    <div className="col-md-9">
-                                        <label className="radio-inline" htmlFor="inline-radio1">
-                                            <input type="radio" id="inline-radio1" name="inline-radios" value="P" onClick={this.onTailleAbadaChanged}/> P 
-                                        </label>
-                                        <label className="radio-inline" htmlFor="inline-radio2" style={{marginLeft: 5 + 'px'}}>
-                                            <input type="radio" id="inline-radio2" name="inline-radios" value="M" onClick={this.onTailleAbadaChanged}/> M 
-                                        </label>
-                                        <label className="radio-inline" htmlFor="inline-radio3" style={{marginLeft: 5 + 'px'}}>
-                                            <input type="radio" id="inline-radio3" name="inline-radios" value="G" onClick={this.onTailleAbadaChanged}/> G 
-                                        </label>
-                                        <label className="radio-inline" htmlFor="inline-radio3" style={{marginLeft: 5 + 'px'}}>
-                                            <input type="radio" id="inline-radio3" name="inline-radios" value="GG" onClick={this.onTailleAbadaChanged}/> GG 
-                                        </label>
-                                    </div>
-                                </div>
-                            </form>
+                    </div>
+                    <div className="form-group row">
+                        <label className="col-md-3 form-control-label">Type de cours</label>
+                        <div className="col-md-9">
+                            <label className="radio-inline" htmlFor="inline-radio1">
+                                <Field component="input" type="radio" id="inline-radio1" name="categorie" value="eveil" /> Eveil
+                                </label>
+                            <label className="radio-inline" htmlFor="inline-radio2" style={{ marginLeft: 5 + 'px' }}>
+                                <Field component="input" type="radio" id="inline-radio2" name="categorie" value="enfants" /> Enfants
+                                </label>
+                            <label className="radio-inline" htmlFor="inline-radio3" style={{ marginLeft: 5 + 'px' }}>
+                                <Field component="input" type="radio" id="inline-radio3" name="categorie" value="adolescents" /> Adolescents
+                                </label>
+                            <label className="radio-inline" htmlFor="inline-radio3" style={{ marginLeft: 5 + 'px' }}>
+                                <Field component="input" type="radio" id="inline-radio3" name="categorie" value="adultes" /> Adultes
+                                </label>
+                        </div>
+                    </div>
+                    <div className="form-group row">
+                        <label className="col-md-3 form-control-label" htmlFor="multiple-select">Cours</label>
+                        <div className="col-md-9">
+                            <Field component="select" id="multiple-select" name="cours" className="form-control" size="5" multiple>
+                                {
+                                    filtered_courses(categorie)
+                                        .map((c, idx) => (
+                                            <option value={c.id}>{c.jour} {c.horaire} à {c.lieu} </option>
+                                        ))
+                                }
+                            </Field>
+                        </div>
+                    </div>
+                    <div className="form-group row">
+                        <label className="col-md-3 form-control-label" htmlFor="select">Corde</label>
+                        <div className="col-md-9">
+                            <Field component="select" id="select" name="corde" className="form-control">
+                                <option value="">Dérouler...</option>
+                                <option value="semCorda">Sem corda</option>
+                                <option value="azul">Azul</option>
+                                <option value="laranja">Laranja</option>
+                                <option value="laranjaMarron">Laranja Marron</option>
+                                <option value="marron">Marron</option>
+                                <option value="marronRoxa">Marron Roxa</option>
+                            </Field>
+                            <span className="help-block">TODO : changer la liste en fonction du type de cours</span>
+                        </div>
+                    </div>
+                    <div className="form-group row">
+                        <label className="col-md-3 form-control-label">Taille d'abada</label>
+                        <div className="col-md-9">
+                            <label className="radio-inline" htmlFor="inline-radio1">
+                                <Field component="input" type="radio" id="inline-radio1" name="taille" value="P" /> P
+                                </label>
+                            <label className="radio-inline" htmlFor="inline-radio2" style={{ marginLeft: 5 + 'px' }}>
+                                <Field component="input" type="radio" id="inline-radio2" name="taille" value="M" /> M
+                                </label>
+                            <label className="radio-inline" htmlFor="inline-radio3" style={{ marginLeft: 5 + 'px' }}>
+                                <Field component="input" type="radio" id="inline-radio3" name="taille" value="G" /> G
+                                </label>
+                            <label className="radio-inline" htmlFor="inline-radio3" style={{ marginLeft: 5 + 'px' }}>
+                                <Field component="input" type="radio" id="inline-radio3" name="taille" value="GG" /> GG
+                                </label>
                         </div>
                     </div>
                 </div>
             </div>
-        )
+        </div>
+    </div>
+)
+
+
+const selector = formValueSelector('inscriptions')
+InfosCapoeira = connect(
+    state => {
+        const categorie = selector(state, 'categorie')
+        return {
+            categorie
+        }
     }
-}
+)(InfosCapoeira)
 
 export default InfosCapoeira;
