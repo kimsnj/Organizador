c95541b60d455d17a7270ef6c84ef32b58280396
Karim Snj <karim.snj@gmail.com>
2017-08-29 17:51:51 +0200

    Add build folder to statics

--- server/settings.py
+++ server/settings.py
@@ -141,6 +141,7 @@ STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = [
+    os.path.join(REACT_APP_DIR, 'build'),
     os.path.join(REACT_APP_DIR, 'build', 'static'),
 ]
 
